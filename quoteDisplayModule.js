b3e6d00b96edcd701b9745352f5a04d7d6c75d4a
Vaibhavi Milind More <vaibmore26@gmail.com>
2019-06-24 19:47:50 -0400

    Add files via upload

---
+++ quoteDisplayModule.js
@@ -0,0 +1,6 @@
+/*function to display quote on Home Page*/
+module.exports.quoteDisplay = function(){
+    //quote to display
+    var quoteOfTheDay="For every minute you are angry you lose sixty seconds of happiness. -Ralph Waldo Emerson";
+    return(quoteOfTheDay);
+};
